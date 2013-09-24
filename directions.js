61101ea06c0d2bb4471c4f723f061587c111df21
kadiyala9999 <kadiyala9999@gmail.com>
2013-09-24 05:41:19 -0500

    ASE
    Venkatesh

---
+++ directions.js
@@ -0,0 +1,38 @@
+$('#control').ready(function() {
+    initialize(17.3660, 78.4760);
+    $('#submit').click(function(){
+          calcRoute();
+            });
+});
+var directionsDisplay;
+var directionsService = new google.maps.DirectionsService();
+function initialize() {
+  directionsDisplay = new google.maps.DirectionsRenderer();
+  var mapOptions = {
+    zoom: 7,
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+    center: new google.maps.LatLng(17.3660, 78.4760)
+  };
+  var map = new google.maps.Map(document.getElementById('map-canvas'),
+      mapOptions);
+  directionsDisplay.setMap(map);
+  directionsDisplay.setPanel(document.getElementById('directions-panel'));
+var control = document.getElementById('control');
+  control.style.display = 'block';
+  map.controls[google.maps.ControlPosition.TOP_CENTER].push(control);
+}
+function calcRoute() {
+  var start = document.getElementById('start').value;
+  var end = document.getElementById('end').value;
+  var request = {
+    origin: start,
+    destination: end,
+    travelMode: google.maps.TravelMode.DRIVING
+  };
+  directionsService.route(request, function(response, status) {
+    if (status == google.maps.DirectionsStatus.OK) {
+      directionsDisplay.setDirections(response);
+    }
+  });
+}
+
